2378f39d2c71d82f0b2ef03cc2f6df56fe91ed68
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-24 20:44:10 +0530

    Added contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,23 +33,25 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [`jobs`, `projects`],
+        singleTypes: [`about`],
         contentTypes: [],
         singleTypes: [],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-webfonts`,
-    //   options: {
-    //     fonts: {
-    //       google: [
-    //         {
-    //           family: "Roboto",
-    //           variants: ["400", "700"],
-    //         },
-    //         { family: "Open Sans" },
-    //       ],
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Roboto",
+              variants: ["400", "700"],
+            },
+            { family: "Open Sans" },
+          ],
+        },
+      },
+    },
   ],
 }
